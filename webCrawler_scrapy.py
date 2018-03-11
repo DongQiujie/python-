eddaafd68d5d0a2b683160d775db76f8f7ade8ab
董秋杰 <dqj1249293688@gmail.com>
2018-03-11 09:18:05 +0800

    scrapy

--- webCrawler_scrapy.py
+++ webCrawler_scrapy.py
@@ -24,7 +24,8 @@ class MofanSpider(scrapy.Spider):
 		for url in urls:
 			yield response.follow(url, callback = self.parse) #继续进行运行，callback后面的是指示跳到哪个函数
         
-# lastly, run this in terminal
+
+	
 # scrapy runspider webCrawler_scrapy.py -o res.json
 #上面这句是运行此脚本的命令，需在terminal里进行运行，scrapy runspider 是运行命令，webCrawler_scrapy是运行的文件名，-o是输出命令
 #将程序运行结果输出到res.json文件中
